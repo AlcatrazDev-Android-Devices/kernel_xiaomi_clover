f55678d6b8d5d647aab502855a19332881ac9fdd
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-17 23:40:15 +0800

    fbdev: backlight debug

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -17,6 +17,9 @@
 #include <linux/fb.h>
 #include <linux/slab.h>
 
+#include <linux/kprobes.h>
+#include <asm/traps.h>
+
 #ifdef CONFIG_PMAC_BACKLIGHT
 #include <asm/backlight.h>
 #endif
@@ -180,9 +183,10 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 		if (brightness > bd->props.max_brightness)
 			rc = -EINVAL;
 		else {
-			pr_debug("set brightness to %lu\n", brightness);
+			pr_err("set brightness to %lu\n", brightness);
 			bd->props.brightness = brightness;
 			rc = backlight_update_status(bd);
+			dump_stack();
 		}
 	}
 	mutex_unlock(&bd->ops_lock);
